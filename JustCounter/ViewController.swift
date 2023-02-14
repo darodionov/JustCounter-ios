e035739d32f4ba2ec9354e7b3dbf948d7cca45f8
Dmitry Rodionov <dr@idrp.ru>
2023-02-14 08:50:38 +0300

    Added Button, Lable and incrementing function.

--- JustCounter/ViewController.swift
+++ JustCounter/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func click(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
 }
-
